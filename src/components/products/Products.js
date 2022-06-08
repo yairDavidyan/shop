a95758de9cb883a138a47ac3a921dfe4be38aaed
Yair Davidyan <yair22218@gmail.com>
2022-06-08 23:56:34 +0300

    Add new project

---
+++ src/components/products/Products.js
@@ -0,0 +1,22 @@
+import Product from "../product/product";
+
+function Products() {
+  return (
+    <>
+      <section class="products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </section>
+    </>
+  );
+}
+
+export default Products;
